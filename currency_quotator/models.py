37c2b3c515c136a3c4d1b001fb3cac43c6fcbc6c
Mario F. Leão <fredericoleaob@gmail.com>
2022-05-06 22:15:15 -0300

    feature: add database model for currency rates.

--- currency_quotator/models.py
+++ currency_quotator/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class CurrencyRate(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    date = models.DateField(null=False, blank=False, unique=True)
+    base = models.CharField(max_length=5)
+    rates = models.TextField()
+
