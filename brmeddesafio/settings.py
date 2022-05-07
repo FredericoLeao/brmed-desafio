56fc89a98ea96243a3a61b3f6fa1abd7cd55de51
Mario F. Leão <fredericoleaob@gmail.com>
2022-05-07 15:55:41 -0300

    fix: redis password for celery

--- brmeddesafio/settings.py
+++ brmeddesafio/settings.py
@@ -126,5 +126,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = 'redis://:w3i@127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://:w3i@127.0.0.1:6379/1'
+CELERY_BROKER_URL = 'redis://:put_redis_pass_here@127.0.0.1:6379/0'
+CELERY_RESULT_BACKEND = 'redis://:put_redis_pass_here@127.0.0.1:6379/1'
